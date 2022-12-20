e8e0937461c987dbc1ad4c389504ccb39a13c6ed
leanollinger <leanollinger@epfedu.fr>
2022-12-20 18:40:10 +0100

    Adding cookies and logs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests 
 
 app = Flask(__name__)
 
@@ -11,8 +12,24 @@ def hello_world():
 window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
 gtag('config', 'UA-250912185-1'); </script>
 """
-    print("Hello Word")
-    return render_template("logs.html")
+    
+    return prefix_google+"Hello Word"
+
+@app.route('/logs', methods=["GET"])
+def log():
+    return render_template("logs.html")+"Let's see on your console"
+
+@app.route('/textbox')
+def textlog():
+    return "Write something"+render_template("textbox.html")+"Let's see on your console after you submit to see what happened"
 
 
+@app.route('/cookie', methods=["GET","POST"])
+def mycookies():
+    req = requests.get("https://www.google.com/")
+    return req.cookies.get_dict() 
 
+@app.route('/cookieganalytics', methods=["GET","POST"])
+def mycookieganalytics():
+    req = requests.get("https://analytics.google.com/analytics/web/#/report-home/a250912185w345031321p281211325")
+    return req.text
